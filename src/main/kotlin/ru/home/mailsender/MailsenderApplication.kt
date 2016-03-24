da3efab3eb44b7b9dbcd36773d1bb6d095cbaf18
znatashka@ya.ru <Cxfcnmt2107>
2016-03-24 10:34:41 +0300

    + launch browser

--- src/main/kotlin/ru/home/mailsender/MailsenderApplication.kt
+++ src/main/kotlin/ru/home/mailsender/MailsenderApplication.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.web.SpringBootServletInitializer
+import java.awt.Desktop
+import java.net.URI
 
 @SpringBootApplication
 open class MailsenderApplication : SpringBootServletInitializer() {
@@ -15,4 +17,15 @@ open class MailsenderApplication : SpringBootServletInitializer() {
 
 fun main(args: Array<String>) {
     SpringApplication.run(MailsenderApplication::class.java, *args)
+    launchBrowser()
+}
+
+fun launchBrowser() {
+    val url = "http://localhost:8080/";
+
+    if (Desktop.isDesktopSupported()) {
+        Desktop.getDesktop().browse(URI(url));
+    } else {
+        Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url)
+    }
 }
